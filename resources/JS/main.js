6fdc0c24a5658c5e5234bf03f3287eab17b5240c
Kioho <yama88x@gmail.com>
2024-04-17 16:19:57 +0200

    Cleaned up the random color code.

--- resources/JS/main.js
+++ resources/JS/main.js
@@ -1,10 +1,22 @@
+function randomNum (num) {
+    let randomizedNum = Math.floor(Math.random() * num);
+    return randomizedNum;
+}
 
-document.getElementById("this").addEventListener("mouseover", changeColor);
+function randomColor () {
+    const colors = ['purple', 'red', 'yellow', 'blue', 'green', 'pink', 'brown'];
+    let randomColor = colors[randomNum(colors.length)];
+    return randomColor;
+}
 
+function changeColor (elem, colorString=randomColor()) {
+    elem.style.color = colorString;
+}
 
-function changeColor() {
-    const colors = ['black', 'red', 'yellow', 'blue', 'green'];
-    let randomizedNum = Math.floor(Math.random() * 5);
-    let randomColor = colors[randomizedNum];
-    document.getElementById("this").style.color = randomColor;
+let idThisElement = document.getElementById("this");
+idThisElement.addEventListener("mouseenter", idThisHandler);
+idThisElement.addEventListener("mouseleave", idThisHandler);
+
+function idThisHandler () {
+    changeColor(idThisElement, randomColor());
 }
